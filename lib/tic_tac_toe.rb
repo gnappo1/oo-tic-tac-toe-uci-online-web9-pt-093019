9aaeff6717d86f3abb8b6107ead82254ce3baa6e
Matteo Piccini <gnappo.1@live.it>
2019-10-17 00:55:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,6 +84,6 @@ class TicTacToe
     until over?
       turn
     end
-    puts won? ? "Congratulations #{winner}" : "Cats game!"
+    puts won? ? "Congratulations #{winner}!" : "Cats game!"
   end
 end
